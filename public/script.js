2cb0edf578eba8565a9d1e49a1e1fe5f119b8350
Ayush-080 <followayush23@gmail.com>
2024-03-31 21:42:31 +0530

    Moved frontend files to public

---
+++ public/script.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        
+        if (this.getAttribute('href') === '#scroll-top') {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        } else {
+            target.scrollIntoView({ behavior: 'smooth' });
+        }
+    });
+});
